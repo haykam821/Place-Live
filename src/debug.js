ca21f7695515ea23bc57884a93f28e84def66604
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-10-13 15:40:55 -0400

    Separate settings management into its own file

--- src/debug.js
+++ src/debug.js
@@ -2,4 +2,5 @@ const debug = require("debug");
 
 module.exports = {
 	main: debug("place-live:main"),
+	settings: debug("place-live:settings"),
 };
--- src/index.js
+++ src/index.js
@@ -8,65 +8,13 @@ screen.style.width = (480 / 854) * window.innerHeight + "px";
 
 const liveIndicator = document.getElementById("live");
 
-let baseSettings = {
-	colors: {
-		/* eslint-disable sort-keys */
-		white: "#FFFFFF",
-		gray: "#E4E4E4",
-		darkgray: "#888888",
-		black: "#222222",
-		pink: "#FFA7D1",
-		red: "#E50000",
-		orange: "#E59500",
-		brown: "#A06A42",
-		yellow: "#E5D900",
-		lime: "#94E044",
-		green: "#02BE01",
-		teal: "#00D3DD",
-		skyblue: "#0083C7",
-		blue: "#0000EA",
-		lavender: "#CF6EE4",
-		purple: "#820080",
-		/* eslint-enable sort-keys */
-	},
-	height: 120,
-	streamID: "",
-	width: 120,
-};
-try {
-	baseSettings = Object.assign(baseSettings, JSON.parse(localStorage.getItem("place-live:settings")));
-} catch (error) {
-	log("Saved settings has invalid JSON, reverting to defaults:", error);
-}
-
-const settings = new Proxy(baseSettings, {
-	set: (target, ...rest) => {
-		const result = Reflect.set(target, ...rest);
-		localStorage.setItem("place-live:settings", JSON.stringify(target));
-		return result;
-	},
-});
+const settings = require("./settings.js");
 
 const board = document.getElementById("board");
 board.style.height = board.clientWidth + "px";
 
 board.height = document.getElementById("boardHeight").innerText = settings.height;
 board.width = document.getElementById("boardWidth").innerText = settings.width;
-
-const settingsInput = document.getElementById("settings");
-settingsInput.value = JSON.stringify(settings, null, "\t");
-settingsInput.addEventListener("input", event => {
-	let newValue = {};
-	try {
-		newValue = JSON.parse(event.target.value);
-	} catch (error) {
-		return log("Invalid JSON in settings input: ", error);
-	}
-
-	for (const key of Object.keys(newValue)) {
-		settings[key] = newValue[key];
-	}
-});
 
 const ctx = board.getContext("2d");
 
